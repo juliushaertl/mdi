badf23890bcf6fb0eced2d1c89d896c899101b89
Thomas Kientz <thomas@kientz.fr>
2019-08-28 21:53:44 +0200

    Add algolia

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>MDI</span>
+        <span class="font-weight-light">SEARCH</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <ais-instant-search
+          :index-name="indexName"
+          :search-client="searchClient"
+        >
+          <ais-configure :hits-per-page.camel="1000" />
+          <ais-search-box>
+            <div slot-scope="{ currentRefinement, isSearchStalled, refine }">
+              <v-text-field
+                prepend-inner-icon="mdi-magnify"
+                label="Search icon"
+                :value="currentRefinement"
+                @input="refine($event)"
+                :loading="isSearchStalled"
+                clearable
+              />
+            </div>
+          </ais-search-box>
+          <ais-hits>
+            <v-row justify="space-between" slot-scope="{ items }" wrap>
+              <v-icon
+                @click="
+                  {
+                  }
+                "
+                v-clipboard:copy="item.name"
+                v-clipboard:success="onCopy"
+                class="ma-4"
+                v-for="item in items"
+                :key="item.objectID"
+                large
+                >mdi-{{ item.name }}</v-icon
+              >
+            </v-row>
+          </ais-hits>
+        </ais-instant-search>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { AisInstantSearch, AisSearchBox } from "vue-instantsearch";
+import algoliasearch from "algoliasearch";
 
 export default {
-  name: "app",
+  name: "App",
+
   components: {
-    HelloWorld
+    AisInstantSearch,
+    AisSearchBox
+  },
+
+  data: () => ({
+    searchClient: algoliasearch(
+      process.env.VUE_APP_ALGOLIA_APP_ID,
+      process.env.VUE_APP_ALGOLIA_API_KEY
+    ),
+    indexName: process.env.VUE_APP_ALGOLIA_INDEX
+  }),
+
+  methods: {
+    onCopy() {
+      this.$toasted.show("Copied !", { duration: 1000, type: "success" });
+    }
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.toasted {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
+import InstantSearch from "vue-instantsearch";
+import VueClipboard from "vue-clipboard2";
+import Toasted from "vue-toasted";
+
+Vue.use(Toasted);
+Vue.use(VueClipboard);
+Vue.use(InstantSearch);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
